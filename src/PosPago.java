53ff6b191f578457d62a403e79fce5b46e1591d4
Lucas <lucascruzestudo@gmail.com>
2023-05-08 20:36:41 -0300

    Criação método - Fazer Chamadas

--- src/PosPago.java
+++ src/PosPago.java
@@ -1,3 +1,26 @@
-public class PosPago {
-    
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+
+public class PosPago extends Assinante 
+{
+    private float assinatura;
+
+    public PosPago(long cpf, String nome, int numero, float assinatura) 
+    {
+        super(cpf, nome, numero);
+        this.assinatura = assinatura;
+    }
+    public void fazerChamada(GregorianCalendar data, int duracao) 
+    {
+        if (this.numChamadas >= this.chamadas.length) 
+        {
+            System.out.println("Assinante com limite de chamadas.");
+            return;
+        }
+
+        this.chamadas[this.numChamadas] = new Chamada(data, duracao);
+        this.assinatura += (1.04) * duracao;
+        this.numChamadas++;
+    }
 }
