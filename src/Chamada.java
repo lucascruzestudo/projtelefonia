a6cd7c63d47f23988214d68d476f4372a6a0e97f
Lucas <lucascruzestudo@gmail.com>
2023-05-08 19:25:25 -0300

    estrutura classe chamada

--- src/Chamada.java
+++ src/Chamada.java
@@ -1,3 +1,24 @@
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class Chamada {
-    
+    private GregorianCalendar data;
+    private int duracao;
+
+    public Chamada(GregorianCalendar data, int duracao) {
+        
+    }
+
+    public GregorianCalendar getData() {
+        return null;
+    }
+    public int getDuracao() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
 }
