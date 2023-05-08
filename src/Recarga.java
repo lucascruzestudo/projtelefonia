fd73a5cd4bf7f87f6b8549585ba0fa27294abe61
Lucas <lucascruzestudo@gmail.com>
2023-05-08 19:29:01 -0300

    estrutura classe recarga

--- src/Recarga.java
+++ src/Recarga.java
@@ -1,3 +1,22 @@
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class Recarga {
-    
+    private GregorianCalendar data;
+    private float valor;
+
+    public Recarga(GregorianCalendar data, float valor) {
+        
+    }
+    public GregorianCalendar getData() {
+        return null;
+    }
+    public float getValor() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
 }
