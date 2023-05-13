a9c93d3cefa22ea7fb1b761c0175a7e4cac2d2f6
Thayna Ferreira <88343113+Thayna-Ferreira@users.noreply.github.com>
2023-05-13 19:20:29 -0300

    Construtor assinante

--- src/Assinante.java
+++ src/Assinante.java
@@ -6,5 +6,16 @@ protected int numChamada = 0;
 
 protected Chamada [] chamada; 
 
+public Assinante (long cpf, String nome, int numero)
+{
+    this.cpf = cpf;
+        this.nome = nome;
+        this.nummero = numero;
+        this.chamadas = new Chamada[5];
 
+}
+
+
+}
+    
 }
