8cc65984da63c7dc42c5510beeb9ac870cbf6ee0
Thayna Ferreira <88343113+Thayna-Ferreira@users.noreply.github.com>
2023-05-13 19:29:21 -0300

    Cadastrar cliente
    verificando o tipo de assinante para solicitação de dados

--- src/Telefonia.java
+++ src/Telefonia.java
@@ -12,3 +12,11 @@ public class Telefonia {
         this.posPago = new PosPago[5];
         this.prePago = new PrePago[5];
     }
+
+    public void cadastrarAssinante() {
+        GerenciadorEntrada gerenciadorEntrada = GerenciadorEntrada.getInstancia();
+        EnumClassificacaoAssinantes tipoAssinante = gerenciadorEntrada.solicitarTipoAssinante();
+
+        boolean is_pospago = tipoAssinante.equals(EnumClassificacaoAssinantes.POSPAGO);
+        boolean is_prepago = tipoAssinante.equals(EnumClassificacaoAssinantes.PREPAGO);
+ // verificando o tipo de  assinante para posteriormente solicitar os dados de assinante específico 
