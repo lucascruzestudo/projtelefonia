22e848ddf4a165710fec4b7e9e343c37866bb097
Thayna Ferreira <88343113+Thayna-Ferreira@users.noreply.github.com>
2023-05-13 19:24:18 -0300

    Atributos classe telefonia

--- src/Telefonia.java
+++ src/Telefonia.java
@@ -1,3 +1,14 @@
+import java.sql.Array;
+import java.util.GregorianCalendar;
+
 public class Telefonia {
-    
-}
+    private int numPrePagos;
+    private int numPosPagos;
+
+    private PrePago[] prePago;
+    private PosPago[] posPago;
+
+    public Telefonia() {
+        this.posPago = new PosPago[5];
+        this.prePago = new PrePago[5];
+    }
