09d635a50b10b648e037065ba4bccd5d16f632ed
Allyson-Santana <santana.allyson459@gmail.com>
2023-05-14 14:36:59 -0300

    feat: cria enum dos meses

---
+++ src/EnumMeses.java
@@ -0,0 +1,14 @@
+public enum EnumMeses {
+    JANEIRO,
+    FEVEREIRO,
+    MARÇO,
+    ABRIL,
+    MAIO,
+    JUNHO,
+    JULHO,
+    AGOSTO,
+    SETEMBRO,
+    OUTUBRO,
+    NOVEMBRO,
+    DEZEMBRO;
+}
