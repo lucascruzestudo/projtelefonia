53b4715223300eefbebf084c826cff4eb7bbe667
Allyson-Santana <santana.allyson459@gmail.com>
2023-05-14 14:38:39 -0300

    feat: cria metodo de imprimir fatura de prepago

--- src/PrePago.java
+++ src/PrePago.java
@@ -1,3 +1,40 @@
 public class PrePago {
     
+    
+    
+    public void imprimirFaturas(int mes) {
+        float valorTotalChamdas = 0f;
+        float valorTotalRecargas = 0f;
+        System.out.println("===============================");
+
+        System.out.println("Dados Pessoais...");
+        System.out.println(this.toString());
+
+        System.out.print("\nChamadas...");
+        for (int i = 0; i < this.numChamadas; i++) {
+            if (mes == this.chamadas[i].getData().get(Calendar.MONTH)) {
+                System.out.println("\n Chamada["+i+"]: " + this.chamadas[i].toString());
+                float _valorTotalChamdas = (float)(this.chamadas[i].getDuracao() * 1.45);
+                System.out.println("  valorChamada:" + _valorTotalChamdas);
+                valorTotalChamdas += _valorTotalChamdas;
+            } else {
+                System.out.print("  Nenhuma...");
+            }
+        }
+
+        System.out.println(" Total gasto em chamadas: "+valorTotalChamdas+" \n");
+
+        System.out.print("\nRecarrega...");
+        for (int i = 0; i < this.numRecargas; i++) {
+            if (mes == this.recargas[i].getData().get(Calendar.MONTH)) {
+                System.out.println("\n Recarga["+i+"]: " + this.recargas[i].toString());
+                float _valorTotalRecargas = this.recargas[i].getValor();
+                valorTotalRecargas += _valorTotalRecargas;
+            } else {
+                System.out.print("  Nenhuma...");
+            }
+        }
+
+        System.out.println("\n Total gasto em recarga: "+valorTotalRecargas+" \n");
+    }
 }
