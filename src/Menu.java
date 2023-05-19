8ac5d36c99d6260af1df260d6bf4251d0df8f739
Juliane de Fatima Neves dos Santos <julianeneves1912@gmail.com>
2023-05-19 15:17:29 -0300

    criando menu

---
+++ src/Menu.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    private GerenciadorEntrada input;
+
+    public Menu() {
+        this.input = GerenciadorEntrada.getInstancia();
+    }
+    
+}
+
